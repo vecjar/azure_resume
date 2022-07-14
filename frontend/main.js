0acd7150fae33f2a28adbfdeac2f04b40fbfe29c
Jarrod <vecjar@gmail.com>
2022-07-14 10:39:53 +1000

    Created azure function api

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const fuctionApi = '';
+const fuctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count =30;
