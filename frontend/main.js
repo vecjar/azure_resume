92f0aa8d9f91ebb9e00ae4fcb1a8b5af70d3aad5
Jarrod <vecjar@gmail.com>
2022-07-14 16:10:40 +1000

    Updated working

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const fuctionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://getresumecounterjv.azurewebsites.net/api/GetResumeCounter?code=5lks3d_j7oRVbRRG7Xwrnx7HCuRBbXiDPdHvK8EbJawWAzFuqRpdZg=='
+const localfuctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count =30;
-    fetch(fuctionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.");
